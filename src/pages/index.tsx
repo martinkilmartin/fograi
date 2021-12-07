c2221034a23eb43d64be3836331eb87a4323f573
sionnach <sionnach.solutions@gmail.com>
2021-12-07 00:23:58 -0500

    Removes Headline from index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,13 +3,11 @@ import heroBGImage from '@img/bg/classifieds-browse.webp'
 import { Container } from '@layouts/Container'
 import { Page } from '@layouts/Page'
 import { Hero } from '@layouts/Hero'
-import { Headlines } from '@components/Headlines'
 
 const HomePage = (): JSX.Element => (
   <Container>
     <Page title={APP_TITLE} heading={TAG_LINE}>
       <Hero bgImageData={heroBGImage} heading={TAG_LINE} />
-      <Headlines />
     </Page>
   </Container>
 )
