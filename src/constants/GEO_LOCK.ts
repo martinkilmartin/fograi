27667f5539a867fcd4812f8ee903148b61bee138
sionnach <sionnach.solutions@gmail.com>
2021-12-06 21:15:27 -0500

    Inital commit

--- src/constants/GEO_LOCK.ts
+++ src/constants/GEO_LOCK.ts
@@ -1,2 +1,2 @@
-export const ALLOWED_COUNTRY = ['IE', 'CA', 'GB']
-export const BLOCKED_COUNTRY = ['US']
+export const ALLOWED_COUNTRY = ['IE', 'CA', 'GB', 'US']
+export const BLOCKED_COUNTRY = ['']
