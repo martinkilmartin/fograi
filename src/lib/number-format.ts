6d273b5c1736515c8cde14d727de657d4b0cdc10
sionnach <sionnach.solutions@gmail.com>
2021-12-15 16:28:40 -0500

    Fix number format issue

--- src/lib/number-format.ts
+++ src/lib/number-format.ts
@@ -1,2 +1,2 @@
 export const numberFormat = (number: number | bigint): string =>
-  Intl.NumberFormat('en-IE', { maximumSignificantDigits: 3 }).format(number)
+  Intl.NumberFormat('en-IE').format(number)
