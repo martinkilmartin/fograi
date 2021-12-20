5c43464b7b2761d4a605d80326dffed741b7f84a
sionnach <sionnach.solutions@gmail.com>
2021-12-20 14:52:59 -0500

    Add flags to footer

--- src/components/Copyright/Copyright.tsx
+++ src/components/Copyright/Copyright.tsx
@@ -1,7 +1,7 @@
 import { BRAND } from '@constants/CONTENT'
 
 const Copyright = (): JSX.Element => (
-  <p>{`© ${new Date().getFullYear()} ${BRAND}`}</p>
+  <p>{`© ${new Date().getFullYear()} 🇮🇪 ${BRAND} 🇨🇦`}</p>
 )
 
 export default Copyright
