980b273165df04284935d3b6692257ffeaa4eee1
sionnach <sionnach.solutions@gmail.com>
2021-12-20 20:56:17 -0500

    Displays sources as cards

--- src/layouts/Sources/Sources.tsx
+++ src/layouts/Sources/Sources.tsx
@@ -7,21 +7,28 @@ type Props = {
 const Sources = ({ title }: Props): JSX.Element => (
   <div className="px-4 text-center">
     <h2 className="w-full mb-4 text-3xl font-extrabold leading-9">{title}</h2>
-    <div className="flex flex-col mx-auto">
+    <div className="flex flex-wrap">
       {Array.from(NewsSources.keys()).map((key, index) => (
-        <p key={index}>
-          <a
-            className="link link-primary"
-            href={NewsSources.get(key)?.url}
-            target={'_blank'}
-            rel="noreferrer"
-          >
-            <b>
-              {NewsSources.get(key)?.name}{' '}
-              <i>({NewsSources.get(key)?.est})</i>
-            </b>
-          </a>
-        </p>
+        <div
+          className="w-full lg:w-1/2 xl:w-1/3 mb-4 card text-center shadow-2xl"
+          key={index}
+        >
+          <div className="card-body">
+            <h2 className="card-title">
+              <a
+                className="link link-primary"
+                href={NewsSources.get(key)?.url}
+                target={'_blank'}
+                rel="noreferrer"
+              >
+                <b>{NewsSources.get(key)?.name}</b>
+              </a>
+            </h2>
+            <p>
+              <i>Estb.&nbsp;{NewsSources.get(key)?.est}</i>
+            </p>
+          </div>
+        </div>
       ))}
     </div>
   </div>
