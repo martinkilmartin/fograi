f9418a3c0c285471f4ce5827dc87d25c601e67df
Martin Kilmartin <martinkilmartin@gmail.com>
2023-07-10 15:55:59 -0400

    Add new logo

--- src/components/MyNavbar/MyNavbar.tsx
+++ src/components/MyNavbar/MyNavbar.tsx
@@ -1,78 +1,24 @@
-import { Link, Navbar, Text, Switch, useTheme, Avatar } from '@nextui-org/react'
+import { Link, Navbar, Switch, useTheme } from '@nextui-org/react'
 import { useTheme as useNextTheme } from 'next-themes'
-import HeadlineLogoFilled from "../SVG/HeadineLogoFilled"
+import Image from 'next/image'
+
 
 export default function MyNavbar(): JSX.Element {
   const { setTheme } = useNextTheme()
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const { isDark, type } = useTheme()
+  const { isDark } = useTheme()
   return (
     <>
       <Navbar isBordered={isDark} shouldHideOnScroll variant="sticky">
         <Navbar.Brand>
-          <Navbar.Toggle aria-label="toggle navigation" showIn="sm" />
-          <HeadlineLogoFilled />
-          <Text b color="inherit" hideIn="xs">
-            <Link href="/">fógr.aí</Link>
-          </Text>
+          <Navbar.Toggle aria-label="toggle navigation" />
         </Navbar.Brand>
-        <Navbar.Content enableCursorHighlight hideIn="sm" variant="underline">
-          <Link href="/oz">
-            <Navbar.Link>
-              <Avatar size="sm"
-                bordered
-                rounded
-                text="🇦🇺" />
-            </Navbar.Link>
-          </Link>
-          <Link href="/ca">
-            <Navbar.Link>
-              <Avatar size="sm"
-                bordered
-                rounded
-                text="🇨🇦" />
-            </Navbar.Link>
-          </Link>
-          <Link href="/in">
-            <Navbar.Link>
-              <Avatar size="sm"
-                bordered
-                rounded
-                text="🇮🇳" />
-            </Navbar.Link>
-          </Link>
-          <Link href="/ie">
-            <Navbar.Link>
-              <Avatar size="sm"
-                bordered
-                rounded
-                text="🇮🇪" />
-            </Navbar.Link>
-          </Link>
-          <Link href="/nz">
-            <Navbar.Link>
-              <Avatar size="sm"
-                bordered
-                rounded
-                text="🇳🇿" />
-            </Navbar.Link>
-          </Link>
-          <Link href="/uk">
-            <Navbar.Link>
-              <Avatar size="sm"
-                bordered
-                rounded
-                text="🇬🇧" />
-            </Navbar.Link>
-          </Link>
-          <Link href="/us">
-            <Navbar.Link>
-              <Avatar size="sm"
-                bordered
-                rounded
-                text="🇺🇸" />
-            </Navbar.Link>
-          </Link>
+        <Navbar.Content>
+          <Link href="/"><Image
+            src="/img/OnlyHeadlines.svg"
+            alt='Only Headlines logo'
+            width={400}
+            height={100} /></Link>
         </Navbar.Content>
         <Navbar.Content>
           <Navbar.Item>
