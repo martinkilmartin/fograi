b08e044f537053ac0a9b0617e04e0af17d477afc
Martin Kilmartin <martinkilmartin@gmail.com>
2023-07-13 21:56:28 -0400

    Use slug for countries

--- src/lib/getHeadlines.ts
+++ src/lib/getHeadlines.ts
@@ -34,7 +34,7 @@ export async function getHeadlines(lastSeen: string | null = null, limit = 8): P
 
 export async function getHeadlinesCountry(lastSeen: string | null = null, limit = 8, country: Countries): Promise<Headline[]> {
     try {
-        const { data, error } = await supabase.rpc('get_paginated_results', {
+        const { data, error } = await supabase.rpc('get_paginated_results_country', {
             page_size: limit,
             country: country,
             last_seen: lastSeen
---
+++ src/pages/[country]/index.tsx
@@ -0,0 +1,78 @@
+import React, { useEffect, useRef } from 'react';
+import { useInfiniteQuery } from 'react-query';
+import { useMediaQuery } from 'react-responsive';
+import { useRouter } from 'next/router';
+import { APP_TITLE, TAG_LINE } from '@constants/CONTENT';
+import { Page } from '@layouts/Page';
+import { getHeadlinesCountry } from '@lib/getHeadlines';
+import { HeadlineList } from '@components/Headlines/HeadlineList';
+import { Headline } from '../../types';
+import { Countries } from 'src/types/countries';
+
+const HomePage: React.FC = () => {
+  const router = useRouter();
+  const { country } = router.query;
+  const isMobile = useMediaQuery({ query: '(max-width: 576px)' });
+  const isTablet = useMediaQuery({ query: '(max-width: 992px)' });
+
+  let limit: number;
+
+  if (isMobile) {
+    limit = 3;
+  } else if (isTablet) {
+    limit = 6;
+  } else {
+    limit = 12;
+  }
+
+  const { data, fetchNextPage, hasNextPage, isFetchingNextPage, status } =
+    useInfiniteQuery<Headline[], Error>(
+      'headlines',
+      ({ pageParam = null }) =>
+        getHeadlinesCountry(pageParam, limit, country as Countries),
+      {
+        getNextPageParam: (lastPage, _pages) =>
+          lastPage[lastPage.length - 1]?.created_at,
+        enabled: router.isReady, // this line was added
+      }
+    );
+
+  const loadMoreRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (
+        !isFetchingNextPage &&
+        hasNextPage &&
+        loadMoreRef.current &&
+        window.innerHeight + window.scrollY >=
+          (loadMoreRef.current.offsetTop + loadMoreRef.current.offsetHeight) *
+            0.5
+      ) {
+        fetchNextPage();
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [fetchNextPage, hasNextPage, isFetchingNextPage]);
+
+  const allHeadlines = data?.pages.flatMap((page) => page);
+  if (!router.isReady) return null;
+  return (
+    <Page title={APP_TITLE} heading={TAG_LINE}>
+      <HeadlineList
+        headlines={allHeadlines}
+        loading={status === 'loading'}
+        fetching={isFetchingNextPage}
+        error={status === 'error' ? new Error() : null}
+      />
+      <div ref={loadMoreRef} />
+    </Page>
+  );
+};
+
+export default HomePage;
