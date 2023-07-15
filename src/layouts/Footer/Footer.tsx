69247d4cfe88f8c0f07483ec11e3778afd2f52e5
Martin Kilmartin <martinkilmartin@gmail.com>
2023-07-14 23:30:36 -0400

    Two col footer

--- src/layouts/Footer/Footer.tsx
+++ src/layouts/Footer/Footer.tsx
@@ -1,26 +1,29 @@
+import { Grid } from '@nextui-org/react';
 import { Copyright } from '@components/Copyright';
-
 import { FOOTER_LINKS } from '@constants/FOOTER_LINKS';
 import Link from 'next/link';
 
 const Footer = (): JSX.Element => (
   <footer>
-    {FOOTER_LINKS.map((linkSection, idx) => (
-      <div
-        key={idx}
-        style={{
-          display: 'flex',
-          flexDirection: 'column',
-        }}
-      >
-        <h3>{linkSection.title}</h3>
-        {linkSection.urls.map((url, idx) => (
-          <Link key={idx} href={url.path}>
-            {url.text}
-          </Link>
-        ))}
-      </div>
-    ))}
+    <Grid.Container justify="center" gap={2}>
+      {FOOTER_LINKS.map((linkSection, idx) => (
+        <Grid key={idx}>
+          <div
+            style={{
+              display: 'flex',
+              flexDirection: 'column',
+            }}
+          >
+            <h3>{linkSection.title}</h3>
+            {linkSection.urls.map((url, idx) => (
+              <Link key={idx} href={url.path}>
+                {url.text}
+              </Link>
+            ))}
+          </div>
+        </Grid>
+      ))}
+    </Grid.Container>
     <div>
       <Copyright />
     </div>
@@ -29,7 +32,7 @@ const Footer = (): JSX.Element => (
         width: 100%;
         bottom: 0;
         background: #f8f9fa;
-        padding: 10px 0;
+        padding-bottom: 10px;
         text-align: center;
       }
     `}</style>
