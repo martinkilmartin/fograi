69ae2dedf3e3051cfdf8cb5fd86282986546c2b4
Martin Kilmartin <martinkilmartin@gmail.com>
2023-07-29 19:33:41 -0400

    Update legal layout

--- src/layouts/Legal/Policy.tsx
+++ src/layouts/Legal/Policy.tsx
@@ -1,63 +1,61 @@
-import { Grid } from '@nextui-org/react'
-import { DescriptionCard } from '.'
+import { Card, Container, Grid } from '@nextui-org/react';
+import { DescriptionCard } from '.';
 
 type Props = {
-  title: string
+  title: string;
   paragraphs: {
-    title?: string
-    subTitle?: string
-    content?: string[]
+    title?: string;
+    subTitle?: string;
+    content?: string[];
     table?: {
-      title: string
-      name: string
-      purpose: string
-    }[]
-  }[]
-}
+      title: string;
+      name: string;
+      purpose: string;
+    }[];
+  }[];
+};
 
 const Policy = ({ title, paragraphs }: Props): JSX.Element => (
-  <div className="px-4">
+  <Container>
     <Grid.Container justify="center">
       <h2>{title}</h2>
     </Grid.Container>
-    <div className="flex flex-col mx-auto">
-      <dl className="w-full">
-        {paragraphs.map((paragraph, i) => (
-          <div key={i}>
-            {paragraph.title && (
-              <dt className="mb-4">
-                <h3 className="text-xl font-semibold text-center">
-                  {paragraph.title}
-                </h3>
-              </dt>
-            )}
-            {paragraph.subTitle && (
-              <dt className="mb-3">
-                <h4 className="text-lg font-semibold text-center">
-                  {paragraph.subTitle}
-                </h4>
-              </dt>
-            )}
-            {paragraph.content &&
-              paragraph.content.map((line, j) => (
-                <dd key={j} className="mb-16">
-                  <p>{line}</p>
-                </dd>
-              ))}
-            {paragraph.table &&
-              paragraph.table.map((description, j) => (
-                <DescriptionCard
-                  key={j}
-                  title={description.title}
-                  subtitle={description.name}
-                  content={description.purpose}
-                />
-              ))}
-          </div>
-        ))}
-      </dl>
-    </div>
-  </div>
-)
+    <Grid.Container justify="center">
+      {paragraphs.map((paragraph, i) => (
+        <Card key={i} css={{ mw: 1200, m: 20 , p: 20}} >
+          {paragraph.title && (
+            <dt className="mb-4">
+              <h3 className="text-xl font-semibold text-center">
+                {paragraph.title}
+              </h3>
+            </dt>
+          )}
+          {paragraph.subTitle && (
+            <dt className="mb-3">
+              <h4 className="text-lg font-semibold text-center">
+                {paragraph.subTitle}
+              </h4>
+            </dt>
+          )}
+          {paragraph.content &&
+            paragraph.content.map((line, j) => (
+              <dd key={j} className="mb-16">
+                <p>{line}</p>
+              </dd>
+            ))}
+          {paragraph.table &&
+            paragraph.table.map((description, j) => (
+              <DescriptionCard
+                key={j}
+                title={description.title}
+                subtitle={description.name}
+                content={description.purpose}
+              />
+            ))}
+        </Card>
+      ))}
+    </Grid.Container>
+  </Container>
+);
 
-export default Policy
+export default Policy;
