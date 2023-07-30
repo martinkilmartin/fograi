889f58a9e6450332f0e22e2aa790dc0b9dcca12d
Martin Kilmartin <martinkilmartin@gmail.com>
2023-07-29 23:23:37 -0400

    Simple source card

--- src/layouts/Sources/Sources.tsx
+++ src/layouts/Sources/Sources.tsx
@@ -1,4 +1,13 @@
-import { Container, Table, Text } from '@nextui-org/react';
+import {
+  Container,
+  Card,
+  Text,
+  Badge,
+  Collapse,
+  Grid,
+  Link,
+  Row,
+} from '@nextui-org/react';
 import { AllNewsSources } from '@constants/NEWS_SOURCES';
 import { flags } from '@constants/FLAGS';
 import { Countries } from 'src/types/countries';
@@ -8,66 +17,98 @@ type Props = {
 };
 
 const Sources = ({ title }: Props): JSX.Element => {
+  function calculateFontSize(text: string, baseSize = 38): number {
+    const reductionFactor = 0.5;
+
+    let newSize = baseSize - text.length * reductionFactor;
+
+    const minimumSize = 12;
+    if (newSize < minimumSize) {
+      newSize = minimumSize;
+    }
+    return newSize;
+  }
   return (
     <Container>
       <h1 style={{ textAlign: 'center' }}>
         {title}&nbsp;({AllNewsSources.size})
       </h1>
-
-      <Table
-        aria-label="List of news sources"
-        css={{
-          height: 'auto',
-          fontFamily: '"Georgia", "Times New Roman", Times, serif',
-        }}
-        lined
-      >
-        <Table.Header>
-          <Table.Column>
-            <Text h2 size={12} weight="bold">
-              Name
-            </Text>
-          </Table.Column>
-          <Table.Column>
-            <Text h2 size={12} weight="bold">
-              Established
-            </Text>
-          </Table.Column>
-        </Table.Header>
-        <Table.Body>
-          {Array.from(AllNewsSources)
-            .sort((a, b) => a[1].est - b[1].est)
-            .map((ns, i) => {
-              const country = ns[0].substring(0, 2).toLowerCase();
-              const cFlag = flags.get(country as Countries);
-              const sourceURL = AllNewsSources.get(ns[0])?.url;
-              const sourceName = AllNewsSources.get(ns[0])?.name;
-              return (
-                <Table.Row key={i}>
-                  <Table.Cell>
-                    <a href={sourceURL} target="_blank" rel="noreferrer">
-                      <Text h2 size={12} weight="bold">
-                        {cFlag}&nbsp;{sourceName ?? ''}
-                      </Text>
-                    </a>
-                  </Table.Cell>
-                  <Table.Cell>
-                    <Text
-                      h2
-                      size={12}
-                      weight="bold"
-                      css={{
-                        textAlign: 'right',
-                      }}
-                    >
-                      {ns[1].est}
-                    </Text>
-                  </Table.Cell>
-                </Table.Row>
-              );
-            })}
-        </Table.Body>
-      </Table>
+      <Grid.Container gap={2} justify="space-evenly" style={{ padding: '0' }}>
+        {Array.from(AllNewsSources)
+          .sort((a, b) => a[1].est - b[1].est)
+          .map((ns, i) => {
+            const country = ns[0].substring(0, 2).toLowerCase();
+            const cFlag = flags.get(country as Countries);
+            const sourceURL = AllNewsSources.get(ns[0])?.url;
+            const sourceName = AllNewsSources.get(ns[0])?.name;
+            const sourceAbout = AllNewsSources.get(ns[0])?.about;
+            const sourceEst = AllNewsSources.get(ns[0])?.est;
+            return (
+              <Grid xs={12} sm={6} md={6} lg={4} xl={3} key={i}>
+                <Card>
+                  <Card.Body>
+                    <Grid.Container>
+                      <Row justify="space-around" align="center">
+                        <Link href={sourceURL} target="_blank" rel="noreferrer">
+                          <Text
+                            h2
+                            size={calculateFontSize(sourceName ?? '')}
+                            weight="bold"
+                            style={{
+                              borderRadius: '0',
+                              fontFamily:
+                                '"Georgia", "Times New Roman", Times, serif',
+                            }}
+                          >
+                            {cFlag}&nbsp;{sourceName ?? ''}
+                          </Text>
+                        </Link>
+                      </Row>
+                      <Grid.Container>
+                        <Grid xs={12}>
+                          <Collapse.Group>
+                            <Collapse title="About">
+                              <Text
+                                size={14}
+                                css={{ mt: '$1' }}
+                                color="#888888"
+                              >
+                                {sourceAbout}
+                              </Text>
+                            </Collapse>
+                          </Collapse.Group>
+                        </Grid>
+                      </Grid.Container>
+                    </Grid.Container>
+                  </Card.Body>
+                  <Card.Footer>
+                    <Grid.Container justify="space-evenly" alignItems="center">
+                      <Grid>
+                        <Text size={18} color="#888888">
+                          Since&nbsp;
+                          <Text b color="foreground" size={18}>
+                            {sourceEst}
+                          </Text>
+                        </Text>
+                      </Grid>
+                      <Grid>
+                        <Link
+                          href={`/source/${sourceName
+                            ?.toLowerCase()
+                            .replaceAll(' ', '-')}`}
+                        >
+                          <Badge color="primary" size="lg">
+                            View All
+                          </Badge>
+                        </Link>
+                      </Grid>
+                    </Grid.Container>
+                  </Card.Footer>
+                </Card>
+              </Grid>
+            );
+          })}
+      </Grid.Container>
     </Container>
   );
 };
