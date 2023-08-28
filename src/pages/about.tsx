ef3b31b4b4eff28d8b02d2360df16a2c37f71af4
Martin Kilmartin <martinkilmartin@gmail.com>
2023-08-27 20:05:46 -0400

    Remove deleted prop

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,14 +1,14 @@
-import { ABOUT_TITLE, TAG_LINE } from '@constants/CONTENT'
-import { Container } from '@layouts/Container'
-import { Page } from '@layouts/Page'
-import { About as AboutLayout } from '@layouts/About'
+import { ABOUT_TITLE, TAG_LINE } from '@constants/CONTENT';
+import { Container } from '@layouts/Container';
+import { Page } from '@layouts/Page';
+import { About as AboutLayout } from '@layouts/About';
 
 const About = (): JSX.Element => (
   <Container>
     <Page title={ABOUT_TITLE} heading={TAG_LINE}>
-      <AboutLayout title={ABOUT_TITLE} />
+      <AboutLayout />
     </Page>
   </Container>
-)
+);
 
-export default About
+export default About;
