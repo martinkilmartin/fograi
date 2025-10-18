6101c5b0c23a69cbdf0982edeeb270672422e954
Martin Kilmartin <martinkilmartin@gmail.com>
2025-10-18 01:00:02 -0400

    Add instagram

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -3,3 +3,4 @@ export { default as RedditCard } from './RedditCard';
 export { default as TwitterCard } from './TwitterCard';
 export { default as FacebookCard } from './FacebookCard';
 export { default as PinterestCard } from './PinterestCard';
+export { default as InstagramCard } from './InstagramCard';
